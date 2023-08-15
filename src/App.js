34ee13254d1ba2948fcf2f650eec009bf54ee875
alejandrogomezdp <alejandrogomezdp@gmail.com>
2023-08-15 19:41:14 +0200

    hay que arreglar lo de pacientes

--- src/App.js
+++ src/App.js
@@ -29,11 +29,33 @@ function App() {
     <div className="container">
       <div className="alert alert-primary" role="alert">
         <img src="https://www.seranil.com/images/web/logo-seranil.png" alt="logo" width="300px" height="130px"></img>
-        <h1>REGISTRO DE TABACO</h1>
+        <h1>Inventorio de tabaco</h1>
       </div>
 
+
       <div className="form-group">
-        <h3>Cantidad de cigarros</h3>
+        <h3>Paquete de tabaco:</h3>
+        <select
+          className="form-control"
+          value={selects}
+          onChange={(event) => { setSelects(event.target.value); }}>
+          <option value="Seleccionar...">Seleccionar...</option>
+          <option value="Lucky Strike Mentolado">Lucky Strike Mentolado</option>
+          <option value="Lucky Strike">Lucky Strike</option>
+          <option value="Malboro">Malboro</option>
+          <option value="Palm Mall">Palm Mall</option>
+          <option value="Malboro Mentolado">Malboro Mentolado</option>
+          <option value="Palm Mall Mentolado">Palm Mall Mentolado</option>
+        </select>
+      </div>
+
+      <div className="alert alert-danger" role="alert">
+        <h3><b>Cantidad de cigarros:</b> {calculation()}<br />
+          <b>Marca y paquete: </b>  {selects} <br /></h3>
+      </div>
+
+      <div className="form-group-2">
+        <h3>Cantidad de cigarros:</h3>
         <input
           type="number"
           className="form"
@@ -47,7 +69,7 @@ function App() {
         />
       </div>
 
-      <div className="form-group">
+      <div className="checkbox">
         <input
           type="checkbox"
           id="paqueteentero"
@@ -76,7 +98,7 @@ function App() {
 
       <div className="alert alert-danger" role="alert">
         <h3><b>Cantidad de cigarros:</b> {calculation()}<br />
-          <b>Marca & Paquete: </b>  {selects} <br /></h3>
+          <b>Marca y paquete: </b>  {selects} <br /></h3>
       </div>
 
       <div className="alert alert-success" role="alert">
